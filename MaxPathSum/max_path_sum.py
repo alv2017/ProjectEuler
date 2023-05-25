501180e7fbc6a1b0ca3e1d737ef0d0fa53244e1a
alv2017 <v.alishauskaite@gmail.com>
2023-05-25 15:56:18 +0300

    MaxPathSum: code formatting issues have been fixed

--- MaxPathSum/max_path_sum.py
+++ MaxPathSum/max_path_sum.py
@@ -1,4 +1,3 @@
-
 class InvalidTriangleData(Exception):
     pass
 
@@ -26,19 +25,22 @@ class Triangle:
         while current_row_index > 0:
             next_row = list(self.row_data[current_row_index - 1])
             for i in range(len(current_row) - 1):
-                next_row[i] += max(current_row[i], current_row[i+1])
+                next_row[i] += max(current_row[i], current_row[i + 1])
             current_row = next_row
             current_row_index -= 1
         return current_row[0]
 
 
 if __name__ == "__main__":
-    t = Triangle(4, [(3,), (7, 4), (2, 4, 6), (8, 5, 9, 3),])
+    t = Triangle(
+        4,
+        [
+            (3,),
+            (7, 4),
+            (2, 4, 6),
+            (8, 5, 9, 3),
+        ],
+    )
     print(t.height)
     print(t.row_data)
     print(t.max_total_sum())
-
-
-
-
-
